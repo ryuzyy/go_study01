c4702ab4fcdbbcb080161012c308af6c22923769
ryuzy_mac <onacool00700@gmail.com>
2021-12-17 10:03:34 +0900

    ponter

--- udemy01/sec05/main.go
+++ udemy01/sec05/main.go
@@ -2,7 +2,27 @@ package main
 
 import "fmt"
 
+func sub01() {
+	var p *int = new(int)
+	fmt.Println(*p)
+	*p++
+	fmt.Println(*p)
+
+	var p2 *int
+	fmt.Println(p2)
+	// *p2++
+	// fmt.Println(p2)
+}
 func main() {
 	var n int = 100
 	fmt.Println(n)
+
+	fmt.Println(&n)
+
+	var p *int = &n
+
+	fmt.Println(p)
+
+	fmt.Println(&p)
+	sub01()
 }
